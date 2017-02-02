a33996cd0f73e1fff4b5ff7ddc843efd3edec69f
danluciano08 <dan.luciano@nessotech.com>
2017-02-02 13:51:11 +0800

    my commit

--- app/src/main/java/com/dev/nessotech/blankproject/navfragments/BaseFragment.java
+++ app/src/main/java/com/dev/nessotech/blankproject/navfragments/BaseFragment.java
@@ -1,31 +1,11 @@
 package com.dev.nessotech.blankproject.navfragments;
 
 import android.support.v4.app.Fragment;
-import android.widget.Toast;
 
-import com.dev.nessotech.blankproject.utils.Delayer;
 
 /**
  * Created by DanLuciano on 12/21/2016.
  */
 
 public class BaseFragment extends Fragment {
-    int secondsDelayBeforeReady = 1;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Delayer.delay(secondsDelayBeforeReady, new Delayer.DelayCallback() {
-            @Override
-            public void afterDelay() {
-                onReady();
-            }
-        });
-    }
-
-    public void onReady()
-    {
-        Toast.makeText(getActivity(), "Fragment Attached", Toast.LENGTH_SHORT).show();
-    }
-
 }
